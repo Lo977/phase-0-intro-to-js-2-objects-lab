075c2d88638d77af0321a1e4ab92493a89c58477
Buddha Lo <lo_977@yahoo.com>
2024-01-18 22:36:19 -0800

    update obj lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sushi",
+    address: "123 A Ave"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeObj = {...employee}
+    newEmployeeObj[key] = value
+    return newEmployeeObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+   const newEmployee = {...employee}
+   delete newEmployee[key]
+   return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
